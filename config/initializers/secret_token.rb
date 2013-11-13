fb9ebd34f56b0b2a9763479618f17b29a2356ab8
mixegen <gennady.miheev@gmail.com>
2013-11-13 17:48:05 +0200

    first blogger commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blogger::Application.config.secret_key_base = 'cf1514a8ae33ff4a47ec24d9ec26b79a81098b553dea5a75452e6c7db3704b8e04dced3481b78d8ab2f4143a64f99db343acaeb4ce3ff6cc27c48d6457528314'
---
+++ vendor/bundle/gems/json-1.8.1/ext/json/ext/generator/siteconf20131113-58256-1tnnq9n.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20131113-58256-1ydtsic"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
